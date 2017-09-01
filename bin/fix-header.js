6cca1bf5653e06780d471bb29fb9aaeab4888507
Johannes Binder <j.binder.x@gmail.com>
2017-09-01 13:26:19 +0200

    Content-Type header fix

--- bin/fix-header.js
+++ bin/fix-header.js
@@ -7,15 +7,15 @@ const gettextParser = require("gettext-parser");
 
 const args = optimist.argv;
 const localesPath = `${args.locales || 'locales'}/*.po`;
-const inputCharset = `${args.inputCharsets || 'utf-8'}`;
+const inputCharset = `${args.inputCharsets || 'UTF-8'}`;
 const filenameToLanguageRegex = `${args.filenameToLanguageRegex || '\\.([^\\.-]+)(.*)'}`;
 
 glob(localesPath, (err, files) => {
   files.map(function(file) {
     var po = gettextParser.po.parse(fs.readFileSync(file), inputCharset);
     if (!po.headers) { po.headers = []; }
-    po.headers['charset'] = po.charset;
-    po.headers['language'] = po.headers['language'] || new RegExp(filenameToLanguageRegex).exec(file)[1];
+    po.headers['Content-Type'] = "text/plain; charset=" + po.charset;
+    po.headers['Language'] = po.headers['Language'] || new RegExp(filenameToLanguageRegex).exec(file)[1];
     var output = gettextParser.po.compile(po);
     fs.writeFileSync(file, output, po.charset);
     console.log("done: " + file + "(" + po.charset + ", " + po.headers['language'] + ")");
